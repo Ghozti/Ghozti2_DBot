629d2f4e99896081b3383477d37833dab206481b
Ghozti <ybalderas1005@gmail.com>
2021-04-22 11:13:25 -0400

    - finished bot

--- src/main/java/Listener.java
+++ src/main/java/Listener.java
@@ -1,3 +1,12 @@
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.listener.message.MessageCreateListener;
 
-public class Listener {
+public class Listener implements MessageCreateListener {
+
+    @Override
+    public void onMessageCreate(MessageCreateEvent event) {
+        if (event.getMessageContent().equalsIgnoreCase("i love you")){
+            event.getChannel().sendMessage("I love you more!");
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,6 @@ public class Main {
 
     public static void main(String[] args){
         DiscordApi api = new DiscordApiBuilder().setToken(Token.TOKEN).login().join();
+        api.addListener(new Listener());
     }
 }
