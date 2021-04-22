a7ca0172a278fddd6f7b59f0d7b67970ec382baa
Ghozti <ybalderas1005@gmail.com>
2021-04-22 11:03:25 -0400

    - no token for you

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,6 @@ import org.javacord.api.DiscordApiBuilder;
 public class Main {
 
     public static void main(String[] args){
-        DiscordApi api = new DiscordApiBuilder().setToken("ODM0Nzk2NDcxNDA0NDYyMTQw.YIGGzA.uKqcWmKVVTciqRpUgprVhjIB84A").login().join();
+        DiscordApi api = new DiscordApiBuilder().setToken(Token.TOKEN).login().join();
     }
 }
