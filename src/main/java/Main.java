61e211098ae292e40f59d13c84ed40eab463a9f0
Ghozti <ybalderas1005@gmail.com>
2021-04-22 10:59:13 -0400

    - ITS ALIVE

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
 public class Main {
 
     public static void main(String[] args){
-
+        DiscordApi api = new DiscordApiBuilder().setToken("ODM0Nzk2NDcxNDA0NDYyMTQw.YIGGzA.uKqcWmKVVTciqRpUgprVhjIB84A").login().join();
     }
 }
