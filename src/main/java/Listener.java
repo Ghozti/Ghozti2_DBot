cf7181c03f3fe729547038465764658f1edf5f24
Ghozti <ybalderas1005@gmail.com>
2021-04-25 15:08:29 -0400

    - restarted the Listener

--- src/main/java/Listener.java
+++ src/main/java/Listener.java
@@ -1,15 +1,13 @@
+
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
-public class Listener implements MessageCreateListener{
+public class Listener implements MessageCreateListener {
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
-
-        if (event.getMessageContent().contains("i love you") || event.getMessageContent().contains("te amo")) {
-            event.getChannel().sendMessage("I love you more!");
-        }else if(event.getMessageContent().contains("$pinthis")) {
-            event.getMessage().pin();
+        if (event.getMessageContent().equalsIgnoreCase("$help")){
+            event.getChannel().sendMessage("");//TODO ADD COMMANDS
         }
     }
 }
