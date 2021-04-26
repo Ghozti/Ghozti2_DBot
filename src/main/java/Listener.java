5f6c3e7608ce8176db6d2f996e4162330c95ac34
Ghozti <ybalderas1005@gmail.com>
2021-04-26 13:27:31 -0400

    - created an embed in Listener

--- src/main/java/Listener.java
+++ src/main/java/Listener.java
@@ -3,13 +3,19 @@ import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 import java.awt.*;
+import java.io.File;
 
 public class Listener implements MessageCreateListener {
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
-        if (event.getMessageContent().equalsIgnoreCase("$embed")){
-
+        if (event.getMessageContent().equalsIgnoreCase("$help")){
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setTitle("test")
+                    .setDescription("Description")
+                    .setAuthor("Author Name")
+                    .addField("A field", "Some text inside the field");
+            event.getChannel().sendMessage(embed);
         }
     }
 }
