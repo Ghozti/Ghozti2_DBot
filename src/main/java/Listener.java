f2133ba3d5ba56897268a29c050f2bd6ca7a7213
Ghozti <ybalderas1005@gmail.com>
2021-04-26 13:12:23 -0400

    - deleted EmbedMaker.java

--- src/main/java/Listener.java
+++ src/main/java/Listener.java
@@ -1,13 +1,15 @@
 
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
+import java.awt.*;
 
 public class Listener implements MessageCreateListener {
 
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
-        if (event.getMessageContent().equalsIgnoreCase("$help")){
-            event.getChannel().sendMessage("");//TODO ADD COMMANDS
+        if (event.getMessageContent().equalsIgnoreCase("$embed")){
+
         }
     }
 }
